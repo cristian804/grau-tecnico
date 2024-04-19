3659a4f0bc45df894367160060549a9fc4871cb6
cristian franklin <163342852+cristian804@users.noreply.github.com>
2024-04-19 11:30:15 -0300

    Create Somar os elementos de um array: Dado um array de números, utilize o foreach para calcular a soma de todos os elementos e exibir o resultado..php
    Somar os elementos de um array:
    Dado um array de números, utilize o foreach para calcular a soma de todos os elementos e exibir o resultado.

---
+++ Somar os elementos de um array: Dado um array de números, utilize o foreach para calcular a soma de todos os elementos e exibir o resultado..php
@@ -0,0 +1,17 @@
+<?php
+
+
+$numeros = array(10, 20, 30, 40, 50);
+
+
+$soma = 0;
+
+
+foreach ($numeros as $numero) {
+  $soma += $numero; 
+}
+
+
+echo "A soma dos elementos do array é: " . $soma;
+
+?>
