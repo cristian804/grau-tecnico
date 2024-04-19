bce66d1ff3ffe166b7791f59571d2b6b818e7aa6
cristian franklin <163342852+cristian804@users.noreply.github.com>
2024-04-19 11:32:36 -0300

    Create Contar a quantidade de elementos em um array: Dado um array de strings, utilize o foreach para contar quantos elementos o array possui e exibir o resultado..php
    Contar a quantidade de elementos em um array:
    Dado um array de strings, utilize o foreach para contar quantos elementos o array possui e exibir o resultado.

---
+++ Contar a quantidade de elementos em um array: Dado um array de strings, utilize o foreach para contar quantos elementos o array possui e exibir o resultado..php
@@ -0,0 +1,17 @@
+<?php
+
+
+$strings = array("Olá", "mundo", "como", "você", "está?");
+
+
+$contagem = 0;
+
+
+foreach ($strings as $string) {
+  $contagem++; 
+}
+
+
+echo "O array possui " . $contagem . " elementos.";
+
+?>
